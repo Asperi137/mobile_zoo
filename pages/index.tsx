6a337e33bbc3b945cce6a590e13ac71a46daf0ef
Asperi137 <jerome_chaput@hotmail.fr>
2022-12-14 13:46:25 +0100

    pour test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import FormConnect from 'components/ui/FormConnect/FormConnect'
 export default function Index () {
   return (
     <div>
+
+      ici pour test 
       <FormConnect />
     </div>
   )
